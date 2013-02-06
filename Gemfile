40718c3c9ef0fa01061afaab19500b72ae82f8a8
Paul Pagel <paul@8thlight.com>
2013-02-06 09:23:13 -0600

    adding the algorithm for stock source data

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :rubygems
 gem 'rake', '0.9.2.2'
 gem 'daemons', '1.1.9'
 gem 'log4r', '1.1.10'
+gem 'fastercsv'
 
 group :test, :development do
   gem 'rspec', '2.11.0'
--- lib/rindlets/stock_source_rindlet.rb
+++ lib/rindlets/stock_source_rindlet.rb
@@ -1,20 +1,45 @@
 require File.expand_path(File.dirname(__FILE__) + "/../rindlet")
+require "fastercsv"
 
 class StockSourceRindlet < Rinda::Rindlet
+  CSV_FILE_PATH = File.expand_path(File.dirname(__FILE__) + "/../../data/")
+  STOCKS = 
+  {  
+    "AAPL"  => 0,
+    "AMZN"  => 0,
+    "BAC"   => 0,
+    "BRK-A" => 0,
+    "CSCO"  => 0,
+    "EBAY"  => 182,
+    "GE"    => 0,
+    "GOOG"  => 1666,
+    "GS"    => 0,
+    "HPQ"   => 0,
+    "MSFT"  => 0,
+    "NVR"   => 0,
+    "XOM"   => 0  
+  }
 
-  def initialize(name, initial_price)
+  def initialize(name)
     @name = name
-    rinda_client.write(["stock", name, initial_price])
+    @row = 0
+    
+    sleep STOCKS[name]
+    @source = []
+    FasterCSV.foreach("#{CSV_FILE_PATH}/#{@name}.csv") do |line|
+      @source << line[1]
+    end
+    
+    rinda_client.write(["stock", name, @source.pop])
   end
 
   def run
     with_tuple(["stock", name]) do |tuple|
       old_price = tuple[2].to_f
-      change_by = [0.87, 0.89, 0.9, 0.95, 0.97, 0.98, 0.99, 1, 1, 1, 1.05, 1.02, 1.13, 1.2, 1.22]
-      new_price = old_price.to_i * change_by[rand(change_by.length)]
+      new_price = @source.pop
       $logger.info "Updating #{@name} from #{old_price} to #{new_price}"
       rinda_client.write(["stock", name, new_price])
-      sleep 5
+      sleep 2
     end
   end
 

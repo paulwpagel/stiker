a02e91e0a049ba1e135026e4e618e4b649998b5f
ryanzverner <ryanzverner@gmail.com>
2013-02-06 10:09:31 -0600

    adding balance request and confirmation capabilities, updated README

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 stiker
 ======
 
+**Tuples**
 
-Tuples
+*Bank Commands:*  
+Register - ["bank", "request", "register", account_name]  
+Buy      - ["bank", "request", "buy", account_name, stock_name, quantity]  
+Sell     - ["bank", "request", "sell", account_name, stock_name, quantity]  
+Balance  - ["bank", "request", "balance", "account_name"]  
 
-Bank Commands:
-Register - ["bank", "request", "register", account_name]
-Buy      - ["bank", "request", "buy", account_name, stock_name, quantity]
-Sell     - ["bank", "request", "sell", account_name, stock_name, quantity]
-
-
-Bank Responses:
-Confirmation  - ["bank", "response", "confirmation", account_name, bank_command, return_value, tuple]
-Failure       - ["bank", "response", "failure", account_name, bank_command, return_value, tuple]
+*Bank Responses:*  
+Confirmation  - ["bank", "response", "confirmation", account_name, bank_command, return_value, tuple]  
+Failure       - ["bank", "response", "failure", account_name, bank_command, return_value, tuple]  
+Game Over	  - ["game_over"]
--- lib/rindlets/bank_rindlet.rb
+++ lib/rindlets/bank_rindlet.rb
@@ -29,17 +29,19 @@ class BankRindlet < Rinda::Rindlet
         action, account_name = tuple[2], tuple[3]
         return_value = nil
         case action
-        when "register"
-          @bank.register(account_name)
-          return_value = 100000
-        when "buy"
-          stock_name, quantity = tuple[4], tuple[5]
-          return_value = current_price = current_price(stock_name)
-          @bank.buy(account_name, stock_name, current_price, quantity)
-        when "sell"
-          stock_name, quantity = tuple[4], tuple[5]
-          return_value = current_price = current_price(stock_name)
-          @bank.sell(account_name, stock_name, current_price, quantity)
+          when "register"
+            @bank.register(account_name)
+            return_value = 100000
+          when "buy"
+            stock_name, quantity = tuple[4], tuple[5]
+            return_value = current_price = current_price(stock_name)
+            @bank.buy(account_name, stock_name, current_price, quantity)
+          when "sell"
+            stock_name, quantity = tuple[4], tuple[5]
+            return_value = current_price = current_price(stock_name)
+            @bank.sell(account_name, stock_name, current_price, quantity)
+          when "balance"
+            return_value = @bank.balance(account_name)
         end
         rinda_client.write(["bank", "response", "confirmation", account_name, action, return_value, tuple])
       rescue InsufficientFunds, InsufficientAssets, StockPriceUnavailable
